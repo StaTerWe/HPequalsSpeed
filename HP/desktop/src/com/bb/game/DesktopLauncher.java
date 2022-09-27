9292066d5e6676fcd1d3116dfeb8b34206d8e348
Tomás Bergonzi <tomi.bergonzi@gmail.com>
2022-09-27 17:01:09 -0300

    first commit

---
+++ HP/desktop/src/com/bb/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.bb.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.bb.game.Principal;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("HPequalsSpeed");
+		new Lwjgl3Application(new Principal(), config);
+	}
+}
